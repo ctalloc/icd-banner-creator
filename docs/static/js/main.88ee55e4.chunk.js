94bdc12b3573e8dca9dfa177ce08e54f73aa9dff
ctalloc <cecilia.tallo@gmail.com>
2021-04-19 16:09:53 +0200

    :tada: Initial commit, clean react file structure.

---
+++ docs/static/js/main.88ee55e4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpicd-banner-creator"]=this["webpackJsonpicd-banner-creator"]||[]).push([[0],{10:function(e,n,r){"use strict";r.r(n);var t=r(1),c=r.n(t),i=r(3),a=r.n(i),o=(r(8),r(0));var s=function(){return Object(o.jsx)("div",{children:Object(o.jsx)("p",{children:"I can't Design Banner Creator"})})};a.a.render(Object(o.jsx)(c.a.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,n,r){}},[[10,1,2]]]);
+//# sourceMappingURL=main.88ee55e4.chunk.js.map
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div>
+      <p>I can't Design Banner Creator</p>
+    </div>
+  );
+}
+
+export default App;
